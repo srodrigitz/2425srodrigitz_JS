cae44d5ec4ad85570bd3f0dc9810a724dba780a4
srodrigitz <srodrigitz@birt.eus>
2024-11-15 20:04:17 +0100

    Update funciones.js

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -39,6 +39,24 @@ function cambProp(obj, prop, valor){
    span.style[prop]= valor;
 }
 
+// efecto teletipo
+
+function teletipo(cad, tmp, iden, cnt) {
+    if (cnt== null) cnt=0;
+    var letra = cad.substr(cnt,1)
+    cuadro=document.getElementById(iden);
+    while (letra == " ")
+      {
+      cnt++;
+      letra += cad.substr(cnt,1);
+     }
+    cuadro.innerHTML += letra;
+    cnt++
+    if (cnt < cad.length){
+       tempor=setTimeout(teletipo,tmp,cad,tmp,iden,cnt); 
+     }
+}
+
 // fecha última actualización
 
 function ultActual()
